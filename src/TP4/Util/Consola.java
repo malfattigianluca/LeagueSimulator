441a3fc1afda40ffb77dc96e81bf863de91b94ed
malfattigianluca <malfattigianluca7@gmail.com>
2025-06-13 20:11:36 -0300

    Create Consola.java

---
+++ src/TP4/Util/Consola.java
@@ -0,0 +1,11 @@
+package TP4.Util;
+
+public class Consola{
+    public static void mostrarMensaje(String mensaje) {
+        System.out.println(mensaje);
+    }
+
+    public static void mostrarError(String mensaje) {
+        System.out.println("Error: " + mensaje);
+    }
+}

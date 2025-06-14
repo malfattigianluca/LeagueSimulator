5fe42eebc4c4484e9884d728b0318e0913bcb25f
malfattigianluca <malfattigianluca7@gmail.com>
2025-06-13 21:44:44 -0300

    Create Enfrentamiento.java

---
+++ src/TP4/Modelo/Enfrentamiento.java
@@ -0,0 +1,11 @@
+package TP4.Modelo;
+
+public class Enfrentamiento {
+    public final Equipo local;
+    public final Equipo visitante;
+
+    public Enfrentamiento(Equipo local, Equipo visitante) {
+        this.local = local;
+        this.visitante = visitante;
+    }
+}

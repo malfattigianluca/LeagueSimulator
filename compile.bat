f64e741f8695cd3337b7324c716713994148333d
malfattigianluca <malfattigianluca7@gmail.com>
2025-06-21 12:37:24 -0300

    Update compile.bat

--- compile.bat
+++ compile.bat
@@ -1,18 +1,18 @@
 @echo off
 echo Compilando clases en orden...
 
-javac -d . src/TP4/Excepciones/TorneoException.java
-javac -d . src/TP4/Util/Validador.java
-javac -d . src/TP4/TDA/ConjuntoGenericoTDA.java
-javac -d . src/TP4/Implementacion/ConjuntoGenericoImpl.java
-javac -d . src/TP4/Modelo/Jugador.java
-javac -d . src/TP4/Modelo/Equipo.java
-javac -d . src/TP4/Modelo/GestorEquipos.java
-javac -d . src/TP4/Modelo/GestorJugadores.java
-javac -d . src/TP4/Modelo/Partido.java
-javac -d . src/TP4/Modelo/SimuladorLiga.java
-javac -d . src/TP4/Modelo/Torneo.java
-javac -d . src/TP4/App.java
+javac -d . src/SimuladorTorneosFutbol_ProyectoFinal/Excepciones/TorneoException.java
+javac -d . src/SimuladorTorneosFutbol_ProyectoFinal/Util/Validador.java
+javac -d . src/SimuladorTorneosFutbol_ProyectoFinal/TDA/ConjuntoGenericoTDA.java
+javac -d . src/SimuladorTorneosFutbol_ProyectoFinal/Implementacion/ConjuntoGenericoImpl.java
+javac -d . src/SimuladorTorneosFutbol_ProyectoFinal/Modelo/Jugador.java
+javac -d . src/SimuladorTorneosFutbol_ProyectoFinal/Modelo/Equipo.java
+javac -d . src/SimuladorTorneosFutbol_ProyectoFinal/Modelo/GestorEquipos.java
+javac -d . src/SimuladorTorneosFutbol_ProyectoFinal/Modelo/GestorJugadores.java
+javac -d . src/SimuladorTorneosFutbol_ProyectoFinal/Modelo/Partido.java
+javac -d . src/SimuladorTorneosFutbol_ProyectoFinal/Modelo/SimuladorLiga.java
+javac -d . src/SimuladorTorneosFutbol_ProyectoFinal/Modelo/Torneo.java
+javac -d . src/SimuladorTorneosFutbol_ProyectoFinal/App.java
 
 echo Compilacion completada.
 pause 
